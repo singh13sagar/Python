9edd087d6ba65bb1b93629398a179f73cdf3bcaa
Sagar Singh <singh13sagar@gmail.com>
2018-10-05 13:05:08 -0400

    went till checkout

--- webscript.py
+++ webscript.py
@@ -1,7 +1,10 @@
 from selenium import webdriver as wd
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+from selenium.webdriver.common.by import By
 import time 
 
 
@@ -11,7 +14,6 @@ driver = wd.Firefox()
 driver.get('https://www.dominos.ca')
 
 assert 'Domino\'s' in driver.title
-
 #driver.findElement(by.className('btn btn--delivery js-delivery')).click()
 elem = driver.find_element_by_xpath('/html/body/header/nav/div[3]/ul/li[1]/a')
 print(elem)
@@ -20,7 +22,7 @@ print(driver.current_url)
 #elem.clear()
 #elem.send_keys('pycon')
 #elem.send_keys(Keys.RETURN)
-
+time.sleep(2)
 assert 'No results found.' not in driver.page_source
 elem2 =  driver.find_element_by_xpath('/html/body/div[2]/div[3]/div/div/form/div/div[1]/div[2]/div[1]/div/label[1]/span[1]')
 print(elem2)
@@ -51,6 +53,34 @@ print(elem7)
 elem7.click()
 print(driver.current_url)
 
+time.sleep(4)
+
+elemPizza = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div/div/div[8]/a/div[2]/h2')
+elemPizza.click()
+
+time.sleep(3)
+
+vegPizza = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div/section/div/div[1]/div/a')
+vegPizza.click()
+time.sleep(5)
+
+
+#driver.implicitly_wait(10)
+
+
+noCustom = driver.find_element_by_xpath('/html/body/div[8]/div/div[2]/form/div[4]/div[3]/div/div[1]/div/button[2]')
+
+noCustom.click()
+time.sleep(3)
+noCheese = driver.find_element_by_xpath('/html/body/div[8]/div/div[2]/form/div[4]/div[3]/div/div[4]/div/button[1]')
+noCheese.click()
+time.sleep(3)
+checkout = driver.find_element_by_xpath('/html/body/div[2]/div[2]/aside/div[2]/div/div[2]/div[1]/a')
+checkout.click()
+
+time.sleep(3)
+skipAd = driver.find_element_by_xpath('/html/body/div[8]/div/a')
+skipAd.click()
 
 
 print(driver.current_url)
